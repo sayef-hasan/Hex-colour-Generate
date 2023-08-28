114b44433436075d89c2fdc686a91a3ac441f87d
Sayef Hasan <sayef.hasan.06@gmail.com>
2023-08-28 20:02:39 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function myColor(){
+    var color = document.getElementById('colorPanel').value;
+    // document.body.style.backgroundColor = color;
+    document.getElementById('box').value = color;
+}
